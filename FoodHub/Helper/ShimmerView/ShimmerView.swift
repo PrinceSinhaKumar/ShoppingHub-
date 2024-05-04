2360b7bccd80e913637fafe1e77ae396cecdbba8
priyanka.m3 <priyanka.m3@glowtouch.com>
2024-05-05 01:20:45 +0530

    Change Description: Add shimmer view on mealList

---
+++ FoodHub/Helper/ShimmerView/ShimmerView.swift
@@ -0,0 +1,18 @@
+//
+//  ShimmerView.swift
+//  FoodHub
+//
+//  Created by Priyanka Mathur on 05/05/24.
+//
+
+import SwiftUI
+
+struct ShimmerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ShimmerView()
+}
