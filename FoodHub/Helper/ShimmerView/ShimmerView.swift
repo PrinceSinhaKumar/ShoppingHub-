babd25a2a16ff28219c99e73841ee065e452a18e
priyanka.m3 <priyanka.m3@glowtouch.com>
2024-05-05 15:53:25 +0530

    Change Description: Add simmer view on meal list screen

--- FoodHub/Helper/ShimmerView/ShimmerView.swift
+++ FoodHub/Helper/ShimmerView/ShimmerView.swift
@@ -8,8 +8,20 @@
 import SwiftUI
 
 struct ShimmerView: View {
+    
+    @State var startPoint: UnitPoint = .init(x: -1.8, y: -1.2)
+    @State var endPoint: UnitPoint = .init(x: 0, y: -0.2)
+    
+    let gradientColor = [AppColor.AppOrange.swiftUIColor.opacity(0.2), Color.white.opacity(0.2), AppColor.AppOrange.swiftUIColor.opacity(0.2)]
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        LinearGradient(colors: gradientColor, startPoint: startPoint, endPoint: endPoint)
+            .onAppear {
+                withAnimation(.easeInOut(duration: 1).repeatForever(autoreverses: false)) {
+                    startPoint = .init(x: 1, y: 1)
+                    endPoint = .init(x: 2.2, y: 2.2)
+                }
+            }
     }
 }
 
